542a017b38e22a1b4181be49962a2e977784c5ef
pedroren <pedroren@gmail.com>
2025-05-18 18:56:52 -0700

    Improving ErrorOrExtensions

--- MoneyTrace.RestBackend/Dto/ErrorsDto.cs
+++ MoneyTrace.RestBackend/Dto/ErrorsDto.cs
@@ -4,8 +4,24 @@ namespace MoneyTrace.RestBackend.Dto;
 
 public static class ErrorOrExtensions
 {
+    /// <summary>
+    /// Converts a list of errors from OrError to a Minimal APIs TypedResults
+    /// </summary>
+    /// <param name="errors">Error list</param>
+    /// <returns>TypedResults.ValidationProblem or TypedResults.Problem</returns>
     public static IResult ToTypedResultsError(this List<Error> errors)
     {
+        //If all the errors are validation errors, return a validation problem
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            Dictionary<string, string[]> validationErrors = errors
+            .GroupBy(error => error.Code)
+            .ToDictionary(
+                group => group.Key,
+                group => group.Select(error => error.Description).ToArray());
+            return TypedResults.ValidationProblem(validationErrors);
+        }
+        //Other kinds of error, return the first one
         var error = errors.FirstOrDefault(); //Only first error for now
         var statusCode = error.Type switch
         {
@@ -16,6 +32,7 @@ public static class ErrorOrExtensions
             ErrorType.Forbidden => StatusCodes.Status403Forbidden,
             _ => StatusCodes.Status500InternalServerError,
         };
+
         return TypedResults.Problem(statusCode: statusCode, title: error.Description);
     }
 }
